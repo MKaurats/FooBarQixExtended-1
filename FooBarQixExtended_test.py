c3717dd81433b2f038ee2f9fed1f14411dfe0a99
MKaurats <57448316+MKaurats@users.noreply.github.com>
2020-04-19 19:37:43 +0300

    First failing unittests
    New project for FooBarQixExtended quiz:
        Added file named: FooBarQixExtended_test.py
        Enviroment: Virtualenv
        To activate venv, run in Win CMD:  C:\PyFilePath\venv\Scripts\activate
        To activate venv, run in Linux terminal:  /home/PyFilePath/venv/bin/activate
        To install pytest, run in terminal: pip3 install pytest
        Settings->Tools->Python integrated tools->Default test runner: pytest
       
    and my first 7 failing pytest unittests by NameError and Assertion error

---
+++ FooBarQixExtended_test.py
@@ -0,0 +1,25 @@
+
+def FooBarCheck(X, ReturnedXVal):
+    ReturnXVal = FooBar(X)
+    assert ReturnXVal == ReturnedXVal
+
+def test_X0():
+    FooBarCheck(0, "Foo Bar")
+
+def test_X1():
+    FooBarCheck(1, "1")
+
+def test_X3():
+    FooBarCheck(3, "Foo")
+
+def test_X5():
+    FooBarCheck(5, "Bar")
+
+def test_X98():
+    FooBarCheck(98, "98")
+
+def test_X99():
+    FooBarCheck(99, "Foo")
+
+def test_X100():
+    FooBarCheck(100, "Bar")
